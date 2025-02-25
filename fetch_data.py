883ab4e94c35c663e852422884df8251b4cd2c3f
Evan Sthapit <evansthapit20@gmail.com>
2025-02-25 21:50:25 +0100

    feat: Add time for fetch and Debug for env

--- fetch_data.py
+++ fetch_data.py
@@ -14,7 +14,17 @@ CONFLUENCE_API_TOKEN = os.environ.get("CONFLUENCE_API_TOKEN")
 
 # Stack Overflow for Teams details
 STK_PAT = os.environ.get("STK_PAT")
-TEAM_SLUG = "onefootball"
+TEAM_SLUG = os.environ.get("TEAM_SLUG")
+
+# # DEBUG
+# print("#" * 220)
+# print("DEBUGGING: Environment Variables:\n")
+# print(f"CONFLUENCE_URL: {CONFLUENCE_URL}")
+# print(f"CONFLUENCE_USERNAME: {CONFLUENCE_USERNAME}")
+# print(f"CONFLUENCE_API_TOKEN: {CONFLUENCE_API_TOKEN}")
+# print(f"STK_PAT: {STK_PAT}")
+# print(f"TEAM_SLUG: {TEAM_SLUG}")
+# print("#" * 220)
 
 # Encode credentials
 auth_header = base64.b64encode(f"{CONFLUENCE_USERNAME}:{CONFLUENCE_API_TOKEN}".encode()).decode()
@@ -26,6 +36,10 @@ headers = {
 }
 
 def fetch_confluence_pages(space_key=None):
+    if CONFLUENCE_API_TOKEN is None:
+        print("Please set the CONFLUENCE_API_TOKEN environment variable.")
+        return []
+
     confluence_data = []
     start = 0
     limit = 1000
@@ -64,6 +78,10 @@ def fetch_confluence_pages(space_key=None):
     return confluence_data
 
 def fetch_stackoverflow_teams():
+    if STK_PAT is None:
+        print("Please set the STK_PAT(Token for Stack Overflow for Teams) environment variable.")
+        return []
+
     headers = {
         "X-API-Access-Token": STK_PAT
     }
@@ -94,7 +112,7 @@ def fetch_stackoverflow_teams():
             
             has_more = data.get('has_more', False)
             page += 1
-            
+
             time.sleep(1)
         else:
             print(f"Error fetching Stack Overflow questions: {response.status_code}")
@@ -108,19 +126,29 @@ def save_data(data, filename="data.json"):
     print(f"Data saved to {filename}")
 
 def main():
+
     print("Fetching Stack Overflow questions...")
     questions = fetch_stackoverflow_teams()
-    
+
     print("Fetching Confluence pages...")
     confluence_pages = []
     with open("spaces.txt", "r") as f:
         spaces = {line.strip() for line in f}
-    
+
     for space in spaces:
         confluence_pages += fetch_confluence_pages(space_key=space)
 
     combined_data = questions + confluence_pages
+    a = len(combined_data)
+    print(f"Total of {a} documents fetched.")
     save_data(combined_data)
 
 if __name__ == "__main__":
+    start_time = time.time()
+    d = time.time() - start_time
+
     main()
+
+    end_time = time.time()
+    b = end_time - start_time - d
+    print(f"Data fetching took {b:.2f} seconds.")
